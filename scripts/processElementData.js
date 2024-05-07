b1c5d32878fc10c7ca0053b86f46a890d658100e
javierra218 <javierra@unicauca.edu.co>
2024-05-06 22:33:32 -0500

    Update

---
+++ scripts/processElementData.js
@@ -0,0 +1,28 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_Ok4CQQPwEe-RGJbFl_eb7QActivity"]="./../../HolaMundoSPEM/capabilitypatterns/resources/Inicial_C5854787_c5854787_Activity.jpeg"
+contentPage.processPage.imageFiles["_hh2XsQPyEe-RGJbFl_eb7Q,_xBTIQQPyEe-RGJbFl_eb7QActivity"]="./../../HolaMundoSPEM/deliveryprocesses/resources/Inicial_C660C43D_1ac101d3_Activity.jpeg"
+contentPage.processPage.imageFiles["_hh2XsQPyEe-RGJbFl_eb7QActivity"]="./../../HolaMundoSPEM/deliveryprocesses/resources/Proceso_A196D542_a196d542_Activity.jpeg"
+contentPage.processPage.imageFiles["_hh2XsQPyEe-RGJbFl_eb7Q,_kwfogQPzEe-RGJbFl_eb7QActivity"]="./../../HolaMundoSPEM/deliveryprocesses/resources/elaboración_9C518C1E_f0b1c9b4_Activity.jpeg"
+contentPage.processPage.elementUrls["_xBTIQQPyEe-RGJbFl_eb7Q"] = ["Inicial","HolaMundoSPEM/deliveryprocesses/Inicial_C660C43D.html"];
+contentPage.processPage.elementUrls["_ch-2UQPwEe-RGJbFl_eb7Q"] = ["Analista","HolaMundoSPEM/roles/analista_DD39ED7F.html"];
+contentPage.processPage.elementUrls["_dOrkQQPwEe-RGJbFl_eb7Q"] = ["Especificación de Requisitos","HolaMundoSPEM/workproducts/especificacin_de_requisitos_9DAD1B79.html"];
+contentPage.processPage.elementUrls["_ch-2UwPwEe-RGJbFl_eb7Q"] = ["Presupuesto","HolaMundoSPEM/workproducts/presupuesto_D1699ADF.html"];
+contentPage.processPage.elementUrls["_dOrkQAPwEe-RGJbFl_eb7Q"] = ["Especificacion","HolaMundoSPEM/tasks/especificacion_683E24E3.html"];
+contentPage.processPage.elementUrls["_kwfogQPzEe-RGJbFl_eb7Q"] = ["Elaboración","HolaMundoSPEM/deliveryprocesses/elaboración_9C518C1E.html"];
+contentPage.processPage.elementUrls["_hh2XsQPyEe-RGJbFl_eb7Q"] = ["Proceso","HolaMundoSPEM/deliveryprocesses/Proceso_A196D542.html"];
+contentPage.processPage.elementUrls["_ufEmoAPzEe-RGJbFl_eb7Q"] = ["Documento Definición Especificación","HolaMundoSPEM/deliveryprocesses/Documento Especificación_AA2253EA.html"];
+contentPage.processPage.elementUrls["_Ok4CQQPwEe-RGJbFl_eb7Q"] = ["Inicial","HolaMundoSPEM/capabilitypatterns/Inicial_C5854787.html"];
+contentPage.processPage.elementUrls["_ch-2UAPwEe-RGJbFl_eb7Q"] = ["Elicitar","HolaMundoSPEM/tasks/elicitar_862F83DC.html"];
+contentPage.processPage.elementUrls["_ch-2VQPwEe-RGJbFl_eb7Q"] = ["Listado de requisitos","HolaMundoSPEM/workproducts/listado_de_requisitos_AE0CAEE3.html"];
